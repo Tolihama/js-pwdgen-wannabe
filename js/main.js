a3297fda5ed1b99674271adc0ff4f2d4e467b126
Tolihama <fra070@gmail.com>
2021-10-20 14:16:42 +0200

    Project setup

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+ * (insicurissimo) password generator
+*/
+
+// - chiedi all’utente il suo nome,
+// - poi chiedi il suo cognome,
+// - poi chiedi il suo colore preferito
+// - infine scrivi sulla pagina i dati ricevuti nel formato: nomecognomecolorepreferito21
