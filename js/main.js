ef6fe42a8e5c42449000b791355df0b4485ab1f9
Tolihama <fra070@gmail.com>
2021-10-20 14:42:38 +0200

    Password generator

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@
 */
 
 // - chiedi all’utente il suo nome,
+const name = prompt('Qual è il tuo nome?');
+
 // - poi chiedi il suo cognome,
+const surname = prompt('Qual è il tuo cognome?');
+
 // - poi chiedi il suo colore preferito
+const prefColor = prompt('Qual è il tuo colore preferito?');
+
 // - infine scrivi sulla pagina i dati ricevuti nel formato: nomecognomecolorepreferito21
+document.getElementById('password').innerHTML = `Password generata: ${name}${surname}${prefColor}21`;
